9a0490e44601260b00e03d089d56fb8e663144e3
Matt Lim <mlimcollege@gmail.com>
2021-11-01 22:49:38 -0700

    Add resources page, adjust header style

---
+++ pages/resources.tsx
@@ -0,0 +1,56 @@
+import ResponsiveContainer from "src/components/ResponsiveContainer";
+import styles from "@/css/pages/Resources.module.css";
+import Body1 from "src/components/text/Body1";
+import Footer from "src/components/Footer";
+import Header from "src/components/Header";
+import ContainerOuter from "src/components/containers/ContainerOuter";
+import HeaderAndDescriptions from "src/components/common/HeaderAndDescriptions";
+
+function Resource({
+  description,
+  href,
+  name,
+}: {
+  description: string;
+  href: string;
+  name: string;
+}) {
+  return (
+    <>
+      <Body1>
+        <a href={href}>{name}</a>
+      </Body1>
+      <Body1>{description}</Body1>
+    </>
+  );
+}
+
+export default function Resources() {
+  return (
+    <ContainerOuter>
+      <Header />
+      <ResponsiveContainer>
+        <div className={styles.containerInner}>
+          <HeaderAndDescriptions
+            header={<>Resources 😍</>}
+            description="A list of other useful tools and resources."
+            help={[
+              <>
+                <a href="https://twitter.com/pencilflip">Lmk</a> if there&apos;s
+                anything you think I should add!
+              </>,
+            ]}
+          />
+          <div className={styles.resources}>
+            <Resource
+              description="Lets you look up token metadata by mint address or token symbol."
+              href="https://naming.bonfida.org/#/token"
+              name="Bonfida token registry"
+            />
+          </div>
+        </div>
+      </ResponsiveContainer>
+      <Footer />
+    </ContainerOuter>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable no-restricted-syntax */
 import styles from "@/css/Header.module.css";
 import Link from "next/link";
+import { useLayoutEffect } from "react";
 import Body1 from "src/components/text/Body1";
 
 function Item({ href, name }: { href: string; name: string }) {
@@ -7,7 +10,7 @@ function Item({ href, name }: { href: string; name: string }) {
     <div className={styles.item}>
       <Link href={href}>
         <div>
-          <Body1>{name}</Body1>
+          <Body1 textAlign="center">{name}</Body1>
         </div>
       </Link>
     </div>
@@ -15,11 +18,37 @@ function Item({ href, name }: { href: string; name: string }) {
 }
 
 export default function Header(): JSX.Element {
+  useLayoutEffect(() => {
+    function highlightFirst() {
+      console.log("resize");
+      const flexContainer = document.getElementById("header-flex");
+      const flexChildren = flexContainer?.children;
+      if (flexChildren == null) {
+        return;
+      }
+      // @ts-ignore
+      const leftPosition = flexChildren[0].offsetLeft;
+      for (const flexChild of flexChildren) {
+        // @ts-ignore
+        if (flexChild.offsetLeft <= leftPosition) {
+          console.log("add firstColumn");
+          flexChild.classList.add("firstColumn");
+        } else {
+          console.log("remove firstColumn");
+          flexChild.classList.remove("firstColumn");
+        }
+      }
+    }
+    highlightFirst();
+    window.addEventListener("resize", highlightFirst);
+  }, []);
+
   return (
-    <div className={styles.header}>
+    <div className={styles.header} id="header-flex">
       <Item href="/pda-finder" name="PDA Finder" />
       <Item href="/metaplex-metadata-finder" name="Metaplex Metadata Finder" />
       <Item href="/token-metadata-finder" name="Token Metadata Finder" />
+      <Item href="/resources" name="Resources" />
     </div>
   );
 }
